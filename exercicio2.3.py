6fe993f9b8cb63f22d7d70fb5b2cc0b9bd93811b
apollows1989 <147453196+apollows1989@users.noreply.github.com>
2023-10-10 22:39:14 -0300

    exer_2.3

--- exercicio2.3.py
+++ exercicio2.3.py
@@ -3,3 +3,18 @@ import cv2
 
 # Adicionando as funções python dos exercícios que se encontram na pasta "codigos_py"
 from codigos_py import app
+
+pasta_saida = "README_FILES/Imagens_processadas"
+
+if not os.path.exists(pasta_saida):
+    os.makedirs(pasta_saida)
+
+# Lendo o arquivo "Diogo.jpg"
+img = app.load_image(f"README_FILES/Imagens_geral/Diogo.jpg", "COLOR")
+
+imagem_processada = app.trocaregioes(img)
+
+# Salvando a imagem processada na pasta "README_FILES/Imagens_processadas"
+cv2.imwrite(
+    f"README_FILES/Imagens_processadas/Diogo_trocaregioes.jpg", imagem_processada
+)
